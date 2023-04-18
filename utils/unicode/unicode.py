d7827a1f841a3a1d8f260488ba38079088f98f57
b0ltzm4nn <113548315+xor-eax-eax-ret@users.noreply.github.com>
2023-04-18 16:31:37 -0400

    Check to see if generated unicode variant is usable before returning

--- utils/unicode/unicode.py
+++ utils/unicode/unicode.py
@@ -7,6 +7,7 @@ import unicodedata
 def get_codepoints(c):
     codepoints = [
         chr(i) for i in range(0x10FFFF)
+        # Don't include anything in Enclosed Alphanumeric Supplement 
         if unicodedata.normalize("NFKC", chr(i)) == c
         and not (0x2460 <= i <= 0x24FF)
         and not (0x1F130 <= i <= 0x1F149)
@@ -25,4 +26,14 @@ def convert_unicode(identifier):
     https://peps.python.org/pep-0672/#normalizing-identifiers
     """
     all_codepoints = [get_codepoints(j) for j in identifier]
-    return "".join([random.choice(i) for i in all_codepoints])
+    variant = ''.join([random.choice(i) for i in all_codepoints])
+    valid = False
+    # Regenerate variant if it's still invalid somehow 
+    # Hacky solution, read Unicode Standard Annex #15 and re-implement later
+    while valid is False:
+        try:
+            exec(f"{variant}=123")
+            valid = True
+        except SyntaxError:
+            variant = ''.join([random.choice(i) for i in all_codepoints])
+    return variant
