aa0c6e90e6326401d0095217d84583184a358b81
b0ltzm4nn <113548315+xor-eax-eax-ret@users.noreply.github.com>
2023-04-19 18:41:44 -0400

    Change flake8 rules

--- unicloak/utils/strings/binary.py
+++ unicloak/utils/strings/binary.py
@@ -19,14 +19,18 @@ def binary(keys, string):
     string_as_bin = _text_to_bits(string)
     # Generate random variable names for the lambda functions 
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    vars = [''.join(random.choice(alphabet.upper()+alphabet.lower()) for i in range(5)) for j in range(5)]
+    vars = [
+        ''.join(random.choice(alphabet.upper() + alphabet.lower()) for i in range(5)) 
+        for j in range(5)
+    ]
     obfus = ''.join([keys[0] if i == "0" else keys[1] for i in string_as_bin])
     decode_func = "(lambda b, e='utf-8', err='surrogatepass': ((n := int(b, 2))).to_bytes((\
             n.bit_length() + 7) // 8, 'big').decode(e, err) or '')"
     decode_func = f"(lambda {vars[0]}, {vars[1]}='utf-8', {vars[2]}='surrogatepass':\
         (({vars[3]} := int({vars[0]}, 2))).to_bytes(({vars[3]}.bit_length() + 7) // 8, 'big')\
         .decode({vars[1]}, {vars[2]}) or '')"
-    to_bin_func = f"(''.join(['0' if {vars[4]} == '{keys[0]}' else '1' for {vars[4]} in '{obfus}']))"
+    to_bin_func = f"(''.join(['0' if {vars[4]} == '{keys[0]}' else '1'\
+        for {vars[4]} in '{obfus}']))"
     final_expr = decode_func + to_bin_func
     final_expr = ast.parse(final_expr).body[0]
     return final_expr
