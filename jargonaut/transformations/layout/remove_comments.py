4050445333233745c7088ba1606e6acdee33e2d0
b0ltzm4nn <113548315+xor-eax-eax-ret@users.noreply.github.com>
2023-05-16 22:24:54 -0400

    Added comment removal

---
+++ jargonaut/transformations/layout/remove_comments.py
@@ -0,0 +1,14 @@
+import libcst as cst
+
+
+class RemoveComments(cst.CSTTransformer):
+    """
+    Removes comments from source code.
+    """
+
+    def leave_Comment(
+        self,
+        original_node: cst.Comment,
+        updated_node: cst.Comment
+    ) -> cst.Comment:
+        return cst.RemoveFromParent()
