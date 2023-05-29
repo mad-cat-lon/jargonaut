8ab968f0fc15100d1649a36467e5fc2b5a6b53e6
b0ltzm4nn <113548315+xor-eax-eax-ret@users.noreply.github.com>
2023-05-29 15:33:49 -0400

    Added progress message

--- jargonaut/transformations/layout/randomize_names.py
+++ jargonaut/transformations/layout/randomize_names.py
@@ -1,4 +1,5 @@
 from libcst.metadata import QualifiedNameProvider, QualifiedNameSource, ScopeProvider, ClassScope
+# from libcst.metadata import TypeInferenceProvider
 import libcst as cst
 import random 
 from typing import Optional
@@ -11,16 +12,26 @@ class RandomizeNames(cst.CSTTransformer):
     inferencing will be done later to do this without breaking
     the obfuscated code
     """
-    METADATA_DEPENDENCIES = (QualifiedNameProvider, ScopeProvider,)
-    
-    def rand_name(self):
-        keys = ["I", "l", "i"]
-        return ''.join(random.choices(keys, k=12))
+    METADATA_DEPENDENCIES = (
+        QualifiedNameProvider, 
+        ScopeProvider,
+        # TypeInferenceProvider
+    )
     
     def __init__(self):
         self.randomize_map = dict()
         self.avoid_names = ["self", "__init__", "main", "super"]
         self.ignore = False
+        self.first_visit = True
+        self.progress_msg = "[-] Randomizing variable names..."
+
+    def rand_name(self):
+        keys = ["I", "l", "i"]
+        return ''.join(random.choices(keys, k=12))
+    
+    # def visit_Call(self, node: cst.Call):
+        # inferred_type = self.get_metadata(TypeInferenceProvider, node)
+        # print(f"{node} -> {inferred_type}")
 
     def visit_Name(self, node: cst.Name) -> Optional[bool]:
         qualified_name = list(self.get_metadata(QualifiedNameProvider, node))
@@ -42,19 +53,31 @@ class RandomizeNames(cst.CSTTransformer):
             False
         )
     
+    def leave_Attribute(
+        self, 
+        original_node: cst.Attribute,
+        updated_node: cst.Attribute
+    ):
+        # qualified_name = list(self.get_metadata(QualifiedNameProvider, original_node))
+        # print(f"{original_node} -> {qualified_name}")
+        return updated_node
+    
     def leave_Name(
             self, 
             original_node: cst.Name,
             updated_node: cst.Name
-    ) -> None:
+    ) -> cst.Name:
+        if self.first_visit is True:
+            print(self.progress_msg)
+            self.first_visit = False
         qualified_name = list(self.get_metadata(QualifiedNameProvider, original_node))
         if self.ignore:
             return updated_node
         else:
             qualified_name = qualified_name[0]
             scope = self.get_metadata(ScopeProvider, original_node)
-            # Don't change names for class methods until we have type inferencing with mypy built in
             if not isinstance(scope, ClassScope):
+                # Don't change names for class methods until we have type inferencing with mypy built in
                 if qualified_name.name in self.randomize_map:
                     # print(f"{qualified_name}.name -> {self.randomize_map[qualified_name.name]}")
                     return updated_node.with_changes(
--- jargonaut/transformations/layout/remove_comments.py
+++ jargonaut/transformations/layout/remove_comments.py
@@ -6,9 +6,16 @@ class RemoveComments(cst.CSTTransformer):
     Removes comments from source code.
     """
 
+    def __init__(self):
+        self.first_visit = True
+        self.progress_msg = "[-] Removing comments..."
+
     def leave_Comment(
         self,
         original_node: cst.Comment,
         updated_node: cst.Comment
     ) -> cst.RemovalSentinel:
+        if self.first_visit is True:
+            print(self.progress_msg)
+            self.first_visit = False
         return cst.RemoveFromParent()
