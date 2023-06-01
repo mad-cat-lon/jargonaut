a63f667baa9da4eead489f2d06399c7ca5014a8c
b0ltzm4nn <113548315+xor-eax-eax-ret@users.noreply.github.com>
2023-05-31 21:10:37 -0400

    Updated progress message and placed scope analysis into separate function

--- jargonaut/transformations/data/const_mba.py
+++ jargonaut/transformations/data/const_mba.py
@@ -23,7 +23,7 @@ class ConstIntToLinearMBA(cst.CSTTransformer):
         self.n_terms_range = n_terms_range
         self.inference = inference
         self.first_visit = True
-        self.progress_msg = "[-] Transforming integer constants to linear MBA forms..."
+        self.progress_msg = "[-] Transforming all integer constants to linear MBA forms..."
         if inference is False:
             ConstIntToLinearMBA.METADATA_DEPENDENCIES = (
                 ParentNodeProvider,
@@ -37,7 +37,66 @@ class ConstIntToLinearMBA(cst.CSTTransformer):
                 PositionProvider,
                 ScopeProvider
             )
-    
+        self.curr_scope = (None, [])
+        self.int_names_available = False
+
+    def get_int_names_in_scope(self, node, scope):
+        # HACK: We get the assignments in the scope that are of type() and appear before the
+        # target node in the source file
+        assigns_in_scope = [
+            assign.node for assign in scope.assignments if isinstance(assign.node, cst.Name)
+            and (
+                ((self.get_metadata(PositionProvider, node).start).line) >
+                ((self.get_metadata(PositionProvider, assign.node).start).line)
+            )
+        ]
+        # print(assigns_in_scope)
+        """        
+        for assign in scope.assignments:
+            # Only take Name() for now
+            if isinstance(assign.node, cst.Name):
+                # Check if it's in scope
+                node_pos = self.get_metadata(PositionProvider, node).start 
+                assign_pos = self.get_metadata(PositionProvider, assign.node).start
+                # HACK: Don't use the assignment unless it appears before our current node
+                if node_pos.line > assign_pos.line:
+                    assigns_in_scope.append(assign.node)
+        """
+        # Do type inferencing here to find Name() nodes that are of type int
+        int_names = []
+        for node in assigns_in_scope:
+            # HACK: When we run PatchReturns() before this transformation,
+            # we insert a Name(value="frame") to do the patching
+            # Pyre doesn't know about this yet so we need to handle the KeyError
+            try:
+                inferred_type = self.get_metadata(TypeInferenceProvider, node)
+                if inferred_type == "int":
+                    int_names.append(node)
+            except KeyError:
+                pass
+        return int_names 
+
+    def visit_Integer(
+        self,
+        node: cst.Integer
+    ):
+        scope = self.get_metadata(ScopeProvider, node)
+        if self.curr_scope[0] != scope:
+            # print("Entering new scope")
+            # print(f"Old scope: {self.curr_scope}")
+            # print(f"New scope {scope} at {cst.parse_module('').code_for_node(node)}")
+            int_names_in_scope = self.get_int_names_in_scope(node, scope)
+            # print(int_names_in_scope)
+            self.curr_scope = (scope, int_names_in_scope)
+            if len(int_names_in_scope) == 0:
+                self.int_names_available = False
+        else:
+            if len(self.curr_scope[1]) == 0:
+                self.int_names_available = False
+            else:
+                self.int_names_available = True
+        return True 
+        
     def leave_Integer(
             self,
             original_node: cst.Integer,
@@ -46,63 +105,23 @@ class ConstIntToLinearMBA(cst.CSTTransformer):
         if self.first_visit:
             print(self.progress_msg)
             self.first_visit = False
-        if self.inference:
-            constant_mba = constant_to_mba(
-                original_node.evaluated_value,
-                n_terms=random.choice(self.n_terms_range),
-                as_obj=False
-            )
-            scope = self.get_metadata(ScopeProvider, original_node)
-            # Let's check if there are any integer variables in the scope we can use for confusion
-            assignments_in_scope = []
-            for assign in scope.assignments:
-                # Only take Name() for now
-                if isinstance(assign.node, cst.Name):
-                    # Check if it's in scope
-                    if assign.node.value in scope:
-                        node_pos = self.get_metadata(PositionProvider, original_node).start 
-                        assign_pos = self.get_metadata(PositionProvider, assign.node).start
-                        # HACK: Don't use the assignment unless it appears before our current node
-                        if node_pos.line > assign_pos.line:
-                            assignments_in_scope.append(assign.node)
-            # Do type inferencing here to find Name() nodes that are of type int
-            int_names = []
-            for node in assignments_in_scope:
-                # HACK: When we run PatchReturns() before this transformation,
-                # we insert a Name(value="frame") to do the patching
-                # Pyre doesn't know about this yet so we need to handle the KeyError
-                try:
-                    inferred_type = self.get_metadata(TypeInferenceProvider, node)
-                    if inferred_type == "int":
-                        int_names.append(node)
-                except KeyError:
-                    pass 
-            if int_names:
+        constant_mba = constant_to_mba(
+            int(original_node.evaluated_value),
+            n_terms=random.choice(self.n_terms_range),
+            as_obj=False
+        )
+        if self.int_names_available:
+            # Replace x and y with variables in the current scope 
+            # TODO: Optimize performance
+            if self.inference:
+                int_names = self.curr_scope[1]
                 # Replace x and y in the constant_mba expression
                 x = random.choice(int_names)
                 y = random.choice(int_names)
                 constant_mba = constant_mba.replace("x", x.value)
                 constant_mba = constant_mba.replace("y", y.value)
                 return cst.parse_expression(constant_mba)
-            else:
-                # print("No names found, inserting statements")
-                # We have a few options here
-                # 1. Just use random integers for x and y
-                # 2. Define integer vars before the expression 
-                # 3. If inside a function, add parameters and edit all calls to include vars
-                # HACK: Doing option 1 for now: inserting statements before a node is finicky 
-                # x_name = ''.join([random.choice("abcdefghihklmnopqrstuv") for _ in range(10)])
-                # y_name = ''.join([random.choice("abcdefghihklmnopqrstuv") for _ in range(10)])
-                x_val = random.randint(0, 1000)
-                y_val = random.randint(0, 1000)
-                constant_mba = constant_mba.replace("x", str(x_val))
-                constant_mba = constant_mba.replace("y", str(y_val))
-                return cst.parse_expression(constant_mba)
         else:
-            constant_mba = constant_to_mba(
-                int(original_node.evaluated_value),
-                as_obj=False
-            )
             x_val = random.randint(0, 1000)
             y_val = random.randint(0, 1000)
             constant_mba = constant_mba.replace("x", str(x_val))
