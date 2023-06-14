ae9a446e7243dff6bc9fa91d7818df990f285fa2
b0ltzm4nn <113548315+xor-eax-eax-ret@users.noreply.github.com>
2023-06-14 16:55:10 -0400

    Added ability to reuse integer function parameters in predicate (WIP)

--- jargonaut/transformations/control/static_preds.py
+++ jargonaut/transformations/control/static_preds.py
@@ -1,6 +1,6 @@
 import libcst as cst 
 # from predicates import mba_preds
-from jargonaut.transformations.control.pred_utils import generate_static_mba_equality_pred
+from jargonaut.transformations.control.pred_utils import generate_static_mba_eq_pred
 from libcst.metadata import ParentNodeProvider, TypeInferenceProvider, PositionProvider
 from libcst.metadata import ScopeProvider
 from yaspin.spinners import Spinners
@@ -16,7 +16,8 @@ class InsertStaticOpaqueMBAPredicates(cst.CSTTransformer):
         self.n_terms_range = n_terms_range
         self.inference = inference
         self.first_visit = True
-        self.progress_msg = "Inserting static opaque predicates..."
+        self.progress_msg = "Inserting static opaque MBA predicates..."
+        self.prob = 0.5
         if inference is False:
             InsertStaticOpaqueMBAPredicates.METADATA_DEPENDENCIES = (
                 ParentNodeProvider,
@@ -30,17 +31,30 @@ class InsertStaticOpaqueMBAPredicates(cst.CSTTransformer):
                 PositionProvider,
                 ScopeProvider
             )
-    
+
     def visit_FunctionDef(self, node):
         if self.first_visit is True:
             self.spinner = kbi_safe_yaspin(Spinners.pipe, text=self.progress_msg, timer=True)
-            self.spinner.start()
+            # self.spinner.start()
             self.first_visit = False
         return True
 
     def leave_FunctionDef(self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef):
         if original_node.name.value != "__init__":
-            mba_pred = generate_static_mba_equality_pred()
+            int_names = []
+            if self.inference:
+                # Just look at function parameters for now, looking at scope is expensive
+                # NOTE: Won't always recognize correct type
+                params = original_node.params.params
+                for param in params:
+                    inferred_type = ""
+                    try:
+                        inferred_type = self.get_metadata(TypeInferenceProvider, param.name)
+                    except KeyError:
+                        inferred_type = ""
+                    if "int" in inferred_type:
+                        int_names.append(param.name)
+            mba_pred = generate_static_mba_eq_pred(int_names=int_names)
             func_body = original_node.body
             body = func_body.body
             if_block = cst.If(
