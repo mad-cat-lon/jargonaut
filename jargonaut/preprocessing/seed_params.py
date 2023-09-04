21b8007e70184269b8c6f661b8e13c6354042fc1
Mustwey <135557972+Mustwey@users.noreply.github.com>
2023-09-04 00:41:46 -0400

    optimized and reduced redundancy in jargonaut/preprocessing/seed_params.py
    I just optimized it, and put some documentation

--- jargonaut/preprocessing/seed_params.py
+++ jargonaut/preprocessing/seed_params.py
@@ -1,112 +1,109 @@
 import libcst as cst
-import random
-from libcst.metadata import QualifiedNameProvider, QualifiedNameSource
-from libcst.metadata import ScopeProvider, ClassScope
-from libcst.metadata import ParentNodeProvider
-from yaspin.spinners import Spinners
+from libcst.metadata import ParentNodeProvider, QualifiedNameProvider, ScopeProvider, QualifiedNameSource, ClassScope
 from yaspin import kbi_safe_yaspin
-
+from yaspin.spinners import Spinners
+import random
 
 class SeedParams(cst.CSTTransformer):
     """
-    Inserts random integer variables (with type hints for pyre) into 
-    function arguments, changing all calls to those functions with the bogus
-    values as well.
+    SeedParams class extends the CSTTransformer from libcst to insert random integer variables
+    (with type hints for pyre) into function arguments. This also updates all calls to those functions
+    with the new random variables.
     """
-    METADATA_DEPENDENCIES = (
-        ParentNodeProvider,
-        QualifiedNameProvider, 
-        ScopeProvider
-    )
+
+    METADATA_DEPENDENCIES = (ParentNodeProvider, QualifiedNameProvider, ScopeProvider)
 
     def __init__(self):
-        self.funcs = []
+        """
+        Initialize SeedParams object.
+        """
+        # Set of function names
+        self.funcs = set()
+
+        # Number of random parameters to add
         self.num_params = 8
+
+        # Flag to indicate first visit
         self.first_visit = True
+
+        # Progress message for spinner
         self.progress_msg = "Seeding function definitions with bogus parameters..."
+
+        # Spinner for indicating progress
         self.spinner = None
-        
+
     def visit_FunctionDef(self, node: cst.FunctionDef):
-        if self.first_visit is True:
-            self.spinner = kbi_safe_yaspin(
-                Spinners.dots12,
-                text=self.progress_msg,
-                timer=True
-            )
+        """
+        Visit each Function Definition in the code and collect its qualified name.
+        Initialize spinner on the first visit.
+        
+        Args:
+            node (cst.FunctionDef): Function definition node.
+            
+        Returns:
+            bool: Always returns True to continue traversal.
+        """
+        if self.first_visit:
+            self.spinner = kbi_safe_yaspin(Spinners.dots12, text=self.progress_msg, timer=True)
             self.spinner.start()
             self.first_visit = False
-        qualified_name = list(self.get_metadata(QualifiedNameProvider, node))
+
+        # Get metadata for the current function definition
+        qualified_names = self.get_metadata(QualifiedNameProvider, node)
         scope = self.get_metadata(ScopeProvider, node)
-        # NOTE: Don't touch class methods for now - we will deal with this along 
-        # with RandomizeNames() once I figure out a robust solution
-        if len(qualified_name) != 0 and not isinstance(scope, ClassScope):
-            qualified_name = qualified_name[0]
-            if qualified_name.source == QualifiedNameSource.LOCAL:
-                if qualified_name.name not in self.funcs:
-                    self.funcs.append(qualified_name.name)
+
+        # Add function name to the set if it's not a class method
+        if qualified_names and not isinstance(scope, ClassScope):
+            qualified_name = next(iter(qualified_names)).name
+            self.funcs.add(qualified_name)
+
         return True
 
-    def leave_FunctionDef(
-        self,
-        original_node: cst.FunctionDef,
-        updated_node: cst.FunctionDef
-    ):
-        qualified_name = list(self.get_metadata(QualifiedNameProvider, original_node))
-        if len(qualified_name) != 0:
-            qualified_name = qualified_name[0]
-            if qualified_name.name in self.funcs:
-                # Add bogus int params with annotations for pyre 
-                new_params = list(original_node.params.params)
-                for _ in range(self.num_params):
-                    new_params.append(
-                        cst.Param(
-                            name=cst.Name(
-                                value=f"seed_int_param_{''.join(random.choices('abcdef', k=10))}",
-                            ),
-                            annotation=cst.Annotation(
-                                annotation=cst.Name(
-                                    value="int"
-                                )
-                            )
-                        )
-                    )
-                params = cst.Parameters(
-                    params=tuple(new_params),
-                    star_arg=original_node.params.star_arg,
-                    kwonly_params=original_node.params.kwonly_params,
-                    star_kwarg=original_node.params.star_kwarg,
-                    posonly_params=original_node.params.posonly_params,
-                    posonly_ind=original_node.params.posonly_ind
-                )
-                # print(original_node.params) 
-                # print(params)
-                return updated_node.with_changes(
-                    params=params
-                )
-        return original_node
+    def leave_FunctionDef(self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef):
+        """
+        Modify the function definition to include new random integer parameters.
         
-    def leave_Call(
-        self,
-        original_node: cst.Call,
-        updated_node: cst.Call
-    ):
-        qualified_name = list(self.get_metadata(QualifiedNameProvider, original_node.func))
-        # inferred_type = self.get_metadata(TypeInferenceProvider, original_node)
-        # print(original_node)
-        # print(f"[CALL] -> {inferred_type}")
-        if len(qualified_name) != 0:
-            qualified_name = qualified_name[0]
-            if qualified_name.name in self.funcs:
-                new_args = list(original_node.args)
-                for _ in range(self.num_params):
-                    new_args.append(
-                        cst.Arg(
-                            value=cst.Integer(
-                                value=str(random.randint(0, 100))
-                            )
-                        )
-                    )
-                return updated_node.with_changes(
-                    args=tuple(new_args)
+        Args:
+            original_node (cst.FunctionDef): Original function definition node.
+            updated_node (cst.FunctionDef): Updated function definition node.
+            
+        Returns:
+            cst.FunctionDef: Modified function definition node.
+        """
+        qualified_names = self.get_metadata(QualifiedNameProvider, original_node)
+        
+        if qualified_names:
+            qualified_name = next(iter(qualified_names)).name
+            if qualified_name in self.funcs:
+                new_params = original_node.params.params + tuple(
+                    cst.Param(
+                        name=cst.Name(value=f"seed_int_param_{''.join(random.choices('abcdef', k=10))}"),
+                        annotation=cst.Annotation(annotation=cst.Name(value="int"))
+                    ) for _ in range(self.num_params)
                 )
+                return updated_node.with_changes(params=original_node.params.with_changes(params=new_params))
+
+        return original_node
+
+    def leave_Call(self, original_node: cst.Call, updated_node: cst.Call):
+        """
+        Modify the function calls to include new random integer arguments for the modified functions.
+        
+        Args:
+            original_node (cst.Call): Original function call node.
+            updated_node (cst.Call): Updated function call node.
+            
+        Returns:
+            cst.Call: Modified function call node.
+        """
+        qualified_names = self.get_metadata(QualifiedNameProvider, original_node.func)
+
+        if qualified_names:
+            qualified_name = next(iter(qualified_names)).name
+            if qualified_name in self.funcs:
+                new_args = original_node.args + tuple(
+                    cst.Arg(value=cst.Integer(value=str(random.randint(0, 100)))) for _ in range(self.num_params)
+                )
+                return updated_node.with_changes(args=new_args)
+
         return updated_node
